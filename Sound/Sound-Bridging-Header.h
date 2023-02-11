79815c56331220b2c52f33ab34f8dc4ec2ccbb45
jan-zelaznog <jan.zelaznog@gmail.com>
2023-02-10 20:55:50 -0600

    initial commit

---
+++ Sound/Sound-Bridging-Header.h
@@ -0,0 +1,7 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+// Aqui vamos a especificar las clases Objective-C que van a estar disponibles para Swift
+
+#import "UIImageAnimatedGIF.h"
--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -6,14 +6,52 @@
 //
 
 import UIKit
+//import SwiftySound
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    var player = AVAudioPlayer()
+    var sonido = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let iv = UIImageView(frame:view.frame.insetBy(dx:80, dy:150))
+        view.addSubview(iv)
+        // obtenemos la URL a la imagen que se agrego como recurso
+        guard let gifURL = Bundle.main.url(forResource: "song", withExtension: "gif") else { return }
+        iv.image = UIImage.animatedImage(withAnimatedGIFURL: gifURL)
+        
+        guard let soundURL = Bundle.main.url(forResource: "MUSIC3", withExtension: "mp3") else { return }
+        do {
+            player = try AVAudioPlayer(contentsOf:soundURL)
+            player.numberOfLoops = -1
+            player.play()
+        } catch {
+            print ("Error abriendo el archivo de sonido " + String(describing: error))
+        }
+        /* IMPLEMENTACION DEL SONIDO CON UN PAQUETE (no funcionó)
+        Sound.play(url: soundURL, numberOfLoops:-1)
+         */
+        sonido = UIButton(frame:CGRect(x: 5, y: 45, width: 44, height: 44))
+        sonido.setBackgroundImage(UIImage(systemName: "speaker.slash.fill"), for: .normal)
+        sonido.setBackgroundImage(UIImage(systemName: "speaker.wave.1.fill"), for: .selected)
+        sonido.addTarget(self, action:#selector(botonTouch), for:.touchUpInside)
+        view.addSubview(sonido)
+    }
+    
+    @objc func botonTouch() {
+        sonido.isSelected = !sonido.isSelected
+        if sonido.isSelected {
+            player.pause()
+        }
+        else {
+            player.play()
+        }
+    }
 }
 
